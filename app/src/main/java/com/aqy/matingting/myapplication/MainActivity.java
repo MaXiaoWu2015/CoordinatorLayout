c1c7609ee53fb9a3732b9ace27f47e763ae64697
matingting <matingting@qiyi.com>
2016-11-16 18:44:49 +0800

    init

---
+++ app/src/main/java/com/aqy/matingting/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.aqy.matingting.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
